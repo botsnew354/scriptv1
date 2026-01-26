4161cb1b3babd719a2d55a57d9cff7a601be52c6
botsnew354 <rubycnc.studios@gmail.com>
2026-01-25 23:53:44 -0300

    v5

--- script.lua
+++ script.lua
@@ -1,6 +1,3 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
 --==================================================
 -- F4X - OBJECT TOOL (EXECUTOR SAFE / GITHUB READY)
 --==================================================
